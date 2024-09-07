84ed2bcda28de7e17a2c70bfc0e5cb726c61c3a4
Md. Raihan Kabir <raihan.kabir.mukit@hotmail.com>
2024-09-08 00:28:12 +0600

    Add project files.

---
+++ DatabaseApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatabaseApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
